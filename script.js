39eaa2fc521a68c9557e54d28601b25426b1bbe4
Aryan <bhattiaryan777@gmail.com>
2026-08-02 23:20:29 +0530

    get location and weather done

--- script.js
+++ script.js
@@ -39,3 +39,64 @@ const updateTime = () => {
 updateTime();
 setInterval(updateTime, 1000)
 
+const getLocation = () => {
+
+    if(!navigator.geolocation){
+        alert("Geolocation is not supported by your browser!")
+        return
+    }
+
+    console.log("Requesting for location...")
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const {latitude, longitude} = position.coords
+        console.log(`Location Found: lat- ${latitude} long-${longitude}`)
+
+        getWeatherByLocation(latitude, longitude)
+    })
+}
+
+getLocation()
+
+const getWeatherByLocation = async (lat, long) => {
+    if(!lat || !long){
+        alert("Unable to get coordinates")
+    }
+
+    try {
+        
+        const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=fbe8226654b37662db331caad38f0810&units=metric`;
+
+        const response = await fetch(URL)
+
+         if (!response.ok) throw new Error('Weather data not found');
+
+         const data = await response.json();
+         console.log(data)
+         displayWeatherData(data)
+
+    } catch (error) {
+        alert("Some error occured fetching location !")
+        console.log(error.message)
+    }
+}
+
+const displayWeatherData = (data) => {
+
+    document.querySelector(".locationName").innerHTML =data.name
+
+    document.querySelector(".temprature").innerHTML = `${data.main.temp}°C`
+
+    document.querySelector(".description").innerHTML = data.weather[0].description
+
+    document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`
+
+    document.querySelector(".wind").innerHTML = `${(data.wind.speed*3.6).toFixed(2)
+    } km/h`
+
+    document.querySelector(".feelsLike").innerHTML = `${data.main.feels_like}°C`
+
+    document.querySelector(".percipitation").innerHTML = `${data.rain ? data.rain['1h'] : 0} mm`
+
+    document.querySelector(".visibility").innerHTML = `${data.visibility / 1000} kms`
+}
