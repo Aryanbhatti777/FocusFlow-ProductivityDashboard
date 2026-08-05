f501a6d63e491d9273b63852a6fccbdd3a61745a
Aryan <bhattiaryan777@gmail.com>
2026-08-05 12:55:19 +0530

    location updated

--- script.js
+++ script.js
@@ -130,11 +130,7 @@ const getWeatherByLocation = async (lat, lon) => {
 
   try {
     const URL =
-      `https://api.openweathermap.org/data/2.5/weather` +
-      `?lat=${lat}` +
-      `&lon=${lon}` +
-      `&appid=61fa14bb69cb911e7c78cfd3d2a4a6d0` +
-      `&units=metric`;
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=61fa14bb69cb911e7c78cfd3d2a4a6d0&units=metric`;
 
     const response = await fetch(URL);
 
